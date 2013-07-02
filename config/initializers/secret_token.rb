a4a59aaa562d19796392cb9245a5c10eca0ddd78
Will Gueble <will.gueble@viget.com>
2013-07-02 13:31:02 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DndClient::Application.config.secret_token = '8a84d9ae71bae737e12e722aaa559fb8a1e68ec9c1b09366d5f6e91bdf7517ac7acf817658eb2b2bbe21f841f100cb51fe1662c5f2d666d4d48847116d32567e'
