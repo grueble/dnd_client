5d260b79a8c05a04c125234d44030bc417d49ea4
Will Gueble <will.gueble@viget.com>
2013-07-18 12:44:20 -0600

    Re-add some accidentally removed files and change and inflection

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -5,7 +5,7 @@
 ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
-  inflect.irregular 'dice', 'die'
+  inflect.irregular 'die', 'dice'
 #   inflect.uncountable %w( fish sheep )
 end
 #
